7bfa6eecf57e25da3455210a0a76ffd3b72ff144
nesanche <sancheznico93@gmail.com>
2017-02-10 10:58:17 -0300

    Added view for alarm_type

--- views/alarms_view.py
+++ views/alarms_view.py
@@ -5,6 +5,7 @@ from schemas import *
 from model import *
 from utils.validators import *
 
+
 class AlarmTypeView(FlaskView):
     route_base = '/alarmType/'
     alarm_type_schema = AlarmTypeSchema()
@@ -21,11 +22,43 @@ class AlarmTypeView(FlaskView):
             raise NotFound("Alarm type not found")
         return jsonify(alarm_type_schema.dump(alarm_type).data)
 
-    def post:
-        return None
+    def post(self):
+        data = request.json
+        data, errors = alarm_type_schema.load(data)
+        if errors:
+            return jsonify(errors), 422
+        alarm_type = AlarmType(
+            name=validate_param("name", data['name'], [(TYPE_VALIDATOR, unicode)], True),
+            description=validate_param("description", data['description'], [(TYPE_VALIDATOR, unicode)], True)
+        )
+        db.session.add(alarm_type)
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            raise
+        return jsonify(alarm_type_schema.dump(alarm_type).data)
 
-    def put:
-        return None
+    def put(self, id):
+        try:
+            alarm_type = AlarmType.query.get_or_404(int(id))
+        except NotFound:
+            raise NotFound("Alarm type not found")
+
+        data = request.json
+        data, errors = alarm_type_schema.load(data)
+        if errors:
+            return jsonify(errors), 422
+        alarm_type.name = validate_param("name", data['name'], [(TYPE_VALIDATOR, unicode)])
+        alarm_type.description = validate_param("description", data['description'], [(TYPE_VALIDATOR, unicode)])
+        db.session.merge(alarm_type)
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            raise
+        return jsonify({"message": "alarm type updated", "alarm_type": alarm_type_schema.dump(alarm_type).data}), 200
+
 
 class AlarmView(FlaskView):
     route_base = '/alarm/'
@@ -84,5 +117,6 @@ class AlarmView(FlaskView):
             db.session.commit()
         except:
             db.session.rollback()
+            raise
         return jsonify({"message":"Alarmer updated", "alarmer": alarmer_schema.dump(alarmer).data}), 200
 
